6ba1c8f5c2c11313994d13f598d5d2c5ecab8f78
Alberto Amoruso <alberto.am@me.com>
2026-01-14 22:52:06 +0100

    Add `DownloaderSelector` class for user-friendly file selection

---
+++ src/Presentation/DownloaderSelector.cs
@@ -0,0 +1,12 @@
+using Humanizer;
+using Spectre.Console;
+
+public static class DownloaderSelector
+{
+	public static List<DownloaderFile> SelectFrom(IEnumerable<DownloaderFile> downloaderFiles) =>
+		AnsiConsole.Prompt(
+			new MultiSelectionPrompt<DownloaderFile>()
+			.AddChoices(downloaderFiles)
+			.UseConverter(downloader => $"{downloader.Name} ({downloader.Size.Bytes().Humanize() ?? "Unknown size"})")
+		);
+}

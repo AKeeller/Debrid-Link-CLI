998be4061d12ecc5681f821fc5143479e4a4d664
Alberto Amoruso <alberto.am@me.com>
2026-01-14 22:31:38 +0100

    Add `DownloaderFile` class

---
+++ src/Domain/Downloader/DownloaderFile.cs
@@ -0,0 +1,12 @@
+public class DownloaderFile
+{
+	public long Created { get; set; }
+	public required string Id { get; set; }
+	public required string Name { get; set; }
+	public required Uri Url { get; set; }
+	public required Uri DownloadUrl { get; set; }
+	public bool Expired { get; set; }
+	public int Chunk { get; set; }
+	public required string Host { get; set; }
+	public long Size { get; set; }
+}

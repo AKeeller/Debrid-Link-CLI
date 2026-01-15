8919b52f20b596466deb2fbfa83f63cca972b45f
Alberto Amoruso <alberto.am@me.com>
2026-01-15 22:31:19 +0100

    Add a title to `TorrentSelector`

--- src/Presentation/Selector/TorrentSelector.cs
+++ src/Presentation/Selector/TorrentSelector.cs
@@ -6,6 +6,7 @@ public static class TorrentSelector
 	public static Torrent SelectFrom(IEnumerable<Torrent> torrents) =>
 		AnsiConsole.Prompt(
 			new SelectionPrompt<Torrent>()
+			.Title("Select a torrent:")
 			.AddChoices(torrents)
 			.UseConverter(torrent => Markup.Escape($"{torrent.Name} ({torrent.TotalSize.Bytes().Humanize() ?? "Unknown size"})"))
 		);

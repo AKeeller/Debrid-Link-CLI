80b99d78a8da4b524a3a80a337121cd78d4c6c77
Alberto Amoruso <alberto.am@me.com>
2026-01-18 11:35:45 +0100

    Add `abstract class ApiCommand` to handle `DebridLinkClient` and API key

---
+++ src/Application/Command/ApiCommand.cs
@@ -0,0 +1,19 @@
+using System.CommandLine;
+
+public abstract class ApiCommand : Command
+{
+	private readonly IApiKeyProvider _apiKeyProvider;
+
+	protected ApiCommand(string name, string description, IApiKeyProvider apiKeyProvider) : base(name, description) => _apiKeyProvider = apiKeyProvider;
+
+	protected void SetActionWithClient(Func<ParseResult, DebridLinkClient, Task<int>> action) =>
+		SetAction(async parseResult =>
+		{
+			var apiKey = _apiKeyProvider.GetApiKey();
+			if (apiKey is null)
+				return 1;
+
+			using var client = new DebridLinkClient(apiKey);
+			return await action(parseResult, client);
+		});
+}
